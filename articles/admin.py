790355343c5ff6dbf4ee102d0f784436f0149145
Maxim Pavlenko 77 <max.pavlenko@gmail.com>
2023-07-04 22:18:25 +0300

    04_07_2024 ver 2

--- articles/admin.py
+++ articles/admin.py
@@ -1,18 +1,34 @@
-from django.contrib import admin
+﻿from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
 from .models import Article, Scope, Tag
 
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        checked = 0
+        for form in self.forms:
+            if form.cleaned_data.get('is_main') is True:
+                checked += 1
+        if checked > 1:
+            raise ValidationError('Основным может быть один раздел')
+            
+        elif checked == 0:
+            raise ValidationError('Укажите основной раздел')
+            
 
 class ScopeInline(admin.TabularInline):
     model = Scope
+    formset = RelationshipInlineFormset
     extra = 0
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'text', 'published_at', 'image']
+    inlines = [ScopeInline,]
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ['id', 'name']
-    inlines = [ScopeInline,]
+    
--- articles/models.py
+++ articles/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+
+
 class Article(models.Model):
 
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    #tags = models.ManyToManyField(Tag, related_name='scope')
 
     class Meta:
         verbose_name = 'Статья'
@@ -15,11 +20,16 @@ class Article(models.Model):
     def __str__(self):
         return self.title
 
-class Tag(models.Model):
-    name = models.CharField(max_length=30)
-    articles = models.ManyToManyField(Article, related_name='scope')
+
+    
 
 class Scope(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name = 'scopes')
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name = 'scopes')
-    is_main = models.BooleanField()
+    is_main = models.BooleanField(verbose_name='Основной')
+
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        verbose_name_plural = 'Тематики статьи'
+
+      

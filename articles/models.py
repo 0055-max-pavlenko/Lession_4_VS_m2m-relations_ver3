9622a71dcad467809222a546f9ba9f3d00b8b8bf
Maxim Pavlenko 77 <max.pavlenko@gmail.com>
2023-07-10 19:53:10 +0300

    10.07.2024 final version

--- articles/models.py
+++ articles/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.name
+    
+
 
 class Article(models.Model):
 
@@ -11,7 +15,6 @@ class Article(models.Model):
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
-    #tags = models.ManyToManyField(Tag, related_name='scope')
 
     class Meta:
         verbose_name = 'Статья'
@@ -27,9 +30,9 @@ class Scope(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name = 'scopes')
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name = 'scopes')
     is_main = models.BooleanField(verbose_name='Основной')
-
+    
     class Meta:
         verbose_name = 'Тематика статьи'
         verbose_name_plural = 'Тематики статьи'
-
+        ordering = ('-is_main','tag__name')
       
